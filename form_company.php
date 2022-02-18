06853a5134c0d643ab3fd7e4024f6e35d7e53718
PorceliBruno <porcelialaniz@gmail.com>
2022-02-17 22:38:22 -0300

    db connect php insert

--- form_company.php
+++ form_company.php
@@ -1,20 +1,32 @@
 <?php
-echo "O valor de CAMPO 1 é: " . $_POST["campo1"];
-echo "<br>O valor de CAMPO 2 é: " . $_POST["campo2"];
 
+$name = $_POST['name'];
+$cnpj = $_POST['cnpj'];
+$mechanil = $_POST['mechanil'];
+$electrical = $_POST['electrical'];
+$painting = $_POST['painting'];
+$tapestry = $_POST['tapestry'];
+$tire = $_POST['tire'];
+$vehicle = $_POST['vehicle'];
+$gas_station = $_POST['gas_station'];
+$observation = $_POST['observation'];
+$public_place = $_POST['public_place'];
+$number = $_POST['number'];
+$district = $_POST['district'];
+$cep = $_POST['cep'];
 
-if (isset($_POST['name'], $_POST['email'])) {
-	$name = $_POST['name'];
-	$email = $_POST['email'];
+$conn_string = "host=sheep port=5432 dbname=test user=lamb password=bar";
+$dbconn = pg_connect($conn_string);
 
-	// show the $name and $email
-	echo "Thanks $name for your subscription.<br>";
-	echo "Please confirm it in your inbox of the email $email.";
-} else {
-	echo 'You need to provide your name and email address.';
-}
+$query = "WITH company_key AS
+(INSERT INTO company (company_name, company_cnpj, company_mechanil, company_electrical, company_painting, company_tapestry, company_tire, company_vehicle, company_gas_station, company_observation) 
+VALUES ($name, $cnpj, $mechanil, $electrical, $painting, $tapestry, $tire, $vehicle, $gas_station, $observation) RETURNING pk_company)
+INSERT INTO address_on (address_public_place, address_number, address_district, address_cep, fk_address_company)
+SELECT $public_place, $number, $district, $cep, company_key.pk_company
+FROM company_key;";
 
+pg_query($dbconn, $query);
 
-
+pg_close($dbconn);
 
 ?>
