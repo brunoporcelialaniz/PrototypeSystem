79e32736a8b06b9cee8617d321d3f88b794fc218
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2022-02-23 15:52:28 -0300

    Dashboard Earnings (Monthly)

--- index.php
+++ index.php
@@ -263,14 +263,23 @@
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Veículos em manutenção
                                             </div>
                                             <div class="row no-gutters align-items-center">
+                                                <?php    
+                                                    include "conexao.php";
+
+                                                    $query_percentage = "SELECT round((COUNT(*)*100) / (SELECT COUNT(*) FROM public.vehicle)) FROM public.maintenance;";
+                                                    $result_percentage = pg_query($dbconn, $query_percentage);
+                                                    $row_percentage = pg_fetch_row($result_percentage); 
+                                                          
+                                                ?>
                                                 <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $row_percentage[0];?>%</div>
                                                 </div>
                                                 <div class="col">
                                                     <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                            aria-valuemax="100"></div>
+                                                        <div class="progress-bar bg-info" role="progressbar" 
+                                                            style="width: <?php echo $row_percentage[0];?>%" aria-valuenow="<?php echo $row_percentage[0];?>" aria-valuemin="0"
+                                                            aria-valuemax="100">
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
@@ -291,7 +300,17 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                 Total de veículos</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">180</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                <?php    
+                                                    include "conexao.php";
+
+                                                    $query = "SELECT count(pk_vehicle)::int FROM public.vehicle;";
+                                                    $result = pg_query($dbconn, $query);
+                                                    $row = pg_fetch_row($result); 
+                                                    echo $row[0];
+                                                          
+                                                ?>  
+                                            </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-car fa-2x text-gray-300"></i>
